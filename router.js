913687b92feae0580b9fbc5d73c13607e1eac47f
Diego Parra <diego.parra.b@usach.cl>
2022-03-30 01:38:50 -0600

    Add Delete An Element

--- router.js
+++ router.js
@@ -35,6 +35,21 @@ router.get('/edit/:id', (req, res)=>{
     })
 })
 
+//Ruta para eliminar registro
+router.get('/delete/:id', (req, res)=>{
+    //Recibir el id
+    const id = req.params.id;
+    //Seleciona de fardo lo capturado en ID
+    conexion.query('DELETE FROM fardo WHERE id=?', [id], (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/');
+        }
+    })
+})
+
+
 //Llamar al CRUD
 const crud = require('./controllers/crud');
 router.post('/save', crud.save);
